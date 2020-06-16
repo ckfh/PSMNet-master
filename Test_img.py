6c651b9eaf63713a7e0a7531f9592ef8803113ae
ckfh <806858698@qq.com>
2020-06-16 16:43:31 +0800

    test

--- Test_img.py
+++ Test_img.py
@@ -13,7 +13,7 @@ from models import *
 import cv2
 from PIL import Image
 
-# 2012 data /media/jiaren/ImageNet/data_scene_flow_2012/testing/
+# 2012 data /media/jiaren/ImageNet/data_scene_flow_2012/testing/11
 
 parser = argparse.ArgumentParser(description='PSMNet')
 parser.add_argument('--KITTI', default='2015',
